81f008a54c3d5b853674a7de9d08e0d0e7a86d6f
Jaci-Xavier <devxvr@gmail.com>
2023-07-04 23:05:37 -0300

    fix: removendo primeira logica

--- backend/src/middlewares/validate.quantity.js
+++ backend/src/middlewares/validate.quantity.js
@@ -1,15 +1,5 @@
 const validateQuantity = (req, res, next) => {
   const sale = req.body;
-  // const saleProductQuantity = sale.map((product) => product.quantity);
-  // const quantityValidate = saleProductQuantity.some((quantity) => quantity <= 0);
-  // const quantityZero = saleProductQuantity.some((quantity) => quantity === undefined);
-
-  // if (!quantityValidate) {
-  //   return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
-  // }
-  // if (!quantityZero) {
-  //   return res.status(400).json({ message: '"quantity" is required' });
-  // }
 
   for (let index = 0; index < sale.length; index += 1) {
     if (sale[index].quantity <= 0) {
